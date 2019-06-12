40f0a8b67b09bb9efee89f11c86ce182270af3ef
Andy Brown <andy@techdad.tech>
2019-06-12 22:21:37 +0100

    looking up works now

--- lib/hiera/backend/dynamodb_backend.rb
+++ lib/hiera/backend/dynamodb_backend.rb
@@ -6,37 +6,47 @@ class Hiera
         begin
           require 'aws-sdk-dynamodb'
           require 'rubygems'
-          if Config.include?(:dynamodb) && !Config[:dynamodb].nil?
-            aws_config = {}
-            if Config[:dynamodb].include?(:access_key_id) && Config[:dynamodb].include?(:secret_access_key)
-              Hiera.debug("Found AWS Access Key #{Config[:dynamodb][:access_key_id]} from configuration")
-              aws_config[:access_key_id] = Config[:dynamodb][:access_key_id]
-              aws_config[:secret_access_key] = Config[:dynamodb][:secret_access_key]
-            end
-            if Config[:dynamodb].include?(:region)
-              Hiera.debug("Found AWS region #{Config[:dynamodb][:region]} from configuration")
-              aws_config[:region] = Config[:dynamodb][:region]
-            end
-            if aws_config.length != 0
-              Hiera.debug('loading DynamoDB backend')
-              ddb = Aws::DynamoDB::Client.new(aws_config)
-            else
-              Hiera.debug('No AWS configuration found, will fall back to env variables or IAM role')
-              ddb = Aws::DynamoDB::Client.new
-            end
-          else
-            Hiera.debug('No configuration found, will fall back to env variables or IAM role')
-            ddb = Aws::DynamoDB::Client.new
-          end
-          table = Config[:dynamodb][:table]
-          Hiera.debug("Hiera dynamodb Client connected. Checking that table: #{table} requested in config exists")
-          exists = ddb.list_tables({})
-          Hiera.debug(exists)
         end
       end
 
       def lookup(key, scope, order_override, resolution_type)
-        answer = nil
+        if Config.include?(:dynamodb) && !Config[:dynamodb].nil?
+          aws_config = {}
+          if Config[:dynamodb].include?(:access_key_id) && Config[:dynamodb].include?(:secret_access_key)
+            Hiera.debug("Found AWS Access Key #{Config[:dynamodb][:access_key_id]} from configuration")
+            aws_config[:access_key_id] = Config[:dynamodb][:access_key_id]
+            aws_config[:secret_access_key] = Config[:dynamodb][:secret_access_key]
+          end
+          if Config[:dynamodb].include?(:region)
+            Hiera.debug("Found AWS region #{Config[:dynamodb][:region]} from configuration")
+            aws_config[:region] = Config[:dynamodb][:region]
+          end
+          if aws_config.length != 0
+            Hiera.debug('loading DynamoDB backend')
+            ddb = Aws::DynamoDB::Client.new(aws_config)
+          else
+            Hiera.debug('No AWS configuration found, will fall back to env variables or IAM role')
+            ddb = Aws::DynamoDB::Client.new
+          end
+        else
+          Hiera.debug('No configuration found, will fall back to env variables or IAM role')
+          ddb = Aws::DynamoDB::Client.new
+        end
+        table = Config[:dynamodb][:table]
+        Hiera.debug("Hiera dynamodb Client connected. Checking that table: #{table} requested in config exists")
+        exists = ddb.list_tables({})
+        Hiera.debug("Looking for #{key}")
+        resp = ddb.get_item({
+          key: {
+            "hiera_key" => key,
+          },
+          table_name: table,
+        })
+        Hiera.debug(resp.to_h)
+        items = resp.to_h[:item]
+        Hiera.debug(items)
+        answer = items["hiera_value"]
+        Hiera.debug(answer)
         return answer
       end
 
